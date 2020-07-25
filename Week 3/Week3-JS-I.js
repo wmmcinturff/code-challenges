ac3be1bba254b4c66df4bfe1b6b75008986e98ac
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-07-25 08:10:01 -0500

    first commit

--- Week 3/Week3-JS-I.js
+++ Week 3/Week3-JS-I.js
@@ -2,148 +2,170 @@
 
 //In these first 6 questions, replace `null` with the answer
 
-//create a string variable, it can contain anything
-const newString = null ;
+//1. create a string variable, it can contain anything
+const newString = null;
 
-//create a number variable, it an be any number
-const newNum = null ;
+//2. create a number variable, it an be any number
+const newNum = null;
 
-//create a boolean variable
-const newBool = null ;
+//3. create a boolean variable
+const newBool = null;
 
-//solve the following math problem
+//4. solve the following math problem
 const newSubtract = 10 - null === 5;
 
-//Solve the following math problem
-const newMultiply = 10 * null === 40 ;
+//5. Solve the following math problem
+const newMultiply = 10 * null === 40;
 
-//Solve the following math problem:
-const newModulo = 21 % 5 === null ;
+//6. Solve the following math problem:
+const newModulo = 21 % 5 === null;
 
-//In the next 22 problems you will compete the function. All of your code will go inside of the function braces. 
+//In the next 22 problems you will compete the function. All of your code will go inside of the function braces.
 //Make sure you use return when the prompt asks you to.
-//hint: console.log() will NOT work. 
+//hint: console.log() will NOT work.
 //Do not change any of the function names
 
+// 7
 function returnString(str) {
-    //simply return the string provided: str
-  }
-  
-  function add(x, y) {
-    // x and y are numbers
-    // add x and y together and return the value
-    // code here
-  }
-  
-  function subtract(x, y) {
-    // subtract y from x and return the value
-    // code here
-  }
-  
-  function multiply(x, y) {
-    // multiply x by y and return the value
-    // code here
-  }
-  
-  function divide(x, y) {
-    // divide x by y and return the value
-    // code here
-  }
-  
-  function areEqual(x, y) {
-    // return true if x and y are the same
-    // otherwise return false
-    // code here
-  }
-  
-  function areSameLength(str1, str2) {
-    // return true if the two strings have the same length
-    // otherwise return false
-    // code here
-  }
-  
-  function lessThanNinety(num) {
-    // return true if the function argument: num , is less than ninety
-    // otherwise return false
-    // code here
-  }
-  
-  function greaterThanFifty(num) {
-    // return true if num is greater than fifty
-    // otherwise return false
-    // code here
-  }
-  
-  function getRemainder(x, y) {
-    // return the remainder from dividing x by y
-    // code here
-  }
-  
-  function isEven(num) {
-    // return true if num is even
-    // otherwise return false
-    // code here
-  }
-  
-  function isOdd(num) {
-    // return true if num is odd
-    // otherwise return false
-    // code here
-  }
-  
-  function square(num) {
-    // square num and return the new value
-    // hint: NOT square root!
-    // code here
-  }
-  
-  function cube(num) {
-    // cube num and return the new value
-    // code here
-  }
-  
-  function raiseToPower(num, exponent) {
-    // raise num to whatever power is passed in as exponent
-    // code here
-  }
-  
-  function roundNumber(num) {
-    // round num and return it
-    // code here
-  }
-  
-  function roundUp(num) {
-    // round num up and return it
-    // code here
-  }
-  
-  function addExclamationPoint(str) {
-    // add an exclamation point to the end of str and return the new string
-    // 'hello world' -> 'hello world!'
-    // code here
-  }
-  
-  function combineNames(firstName, lastName) {
-    // return firstName and lastName combined as one string and separated by a space.
-    // 'NSS', 'Bootcamp' -> 'NSS Bootcamp'
-    // code here
-  }
-  
-  function getGreeting(name) {
-    // Take the name string and concatenate other strings onto it so it takes the following form:
-    // 'Sam' -> 'Hello Sam!'
-    // code here
-  }
-  
-  // The next three questions will have you implement math area formulas. 
-  // If you can't remember these area formulas then head over to Google.
-   
-  function getRectangleArea(length, width) {
-    // return the area of the rectangle by using length and width
-    // code here
-  }
-  
-  function getTriangleArea(base, height) {
-    // return the area of the triangle by using base and height
-    // code here
-  }
+  //simply return the string provided: str
+}
+
+// 8
+function add(x, y) {
+  // x and y are numbers
+  // add x and y together and return the value
+  // code here
+}
+
+// 9
+function subtract(x, y) {
+  // subtract y from x and return the value
+  // code here
+}
+
+// 10
+function multiply(x, y) {
+  // multiply x by y and return the value
+  // code here
+}
+
+// 11
+function divide(x, y) {
+  // divide x by y and return the value
+  // code here
+}
+
+// 12
+function areEqual(x, y) {
+  // return true if x and y are the same
+  // otherwise return false
+  // code here
+}
+
+// 13
+function areSameLength(str1, str2) {
+  // return true if the two strings have the same length
+  // otherwise return false
+  // code here
+}
+
+// 14
+function lessThanNinety(num) {
+  // return true if the function argument: num , is less than ninety
+  // otherwise return false
+  // code here
+}
+
+// 15
+function greaterThanFifty(num) {
+  // return true if num is greater than fifty
+  // otherwise return false
+  // code here
+}
+
+// 16
+function getRemainder(x, y) {
+  // return the remainder from dividing x by y
+  // code here
+}
+
+// 17
+function isEven(num) {
+  // return true if num is even
+  // otherwise return false
+  // code here
+}
+
+// 18
+function isOdd(num) {
+  // return true if num is odd
+  // otherwise return false
+  // code here
+}
+
+// 19
+function square(num) {
+  // square num and return the new value
+  // hint: NOT square root!
+  // code here
+}
+
+// 20
+function cube(num) {
+  // cube num and return the new value
+  // code here
+}
+
+// 21
+function raiseToPower(num, exponent) {
+  // raise num to whatever power is passed in as exponent
+  // code here
+}
+
+// 22
+function roundNumber(num) {
+  // round num and return it
+  // code here
+}
+
+// 23
+function roundUp(num) {
+  // round num up and return it
+  // code here
+}
+
+// 24
+function addExclamationPoint(str) {
+  // add an exclamation point to the end of str and return the new string
+  // 'hello world' -> 'hello world!'
+  // code here
+}
+
+// 25
+function combineNames(firstName, lastName) {
+  // return firstName and lastName combined as one string and separated by a space.
+  // 'NSS', 'Bootcamp' -> 'NSS Bootcamp'
+  // code here
+}
+
+// 26
+function getGreeting(name) {
+  // Take the name string and concatenate other strings onto it so it takes the following form:
+  // 'Sam' -> 'Hello Sam!'
+  // code here
+}
+
+// The next three questions will have you implement math area formulas.
+// If you can't remember these area formulas then head over to Google.
+
+// 27
+function getRectangleArea(length, width) {
+  // return the area of the rectangle by using length and width
+  // code here
+}
+
+// 28
+function getTriangleArea(base, height) {
+  // return the area of the triangle by using base and height
+  // code here
+}
